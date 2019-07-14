d0150fa84ae8e23d0c7490cb6cb3e494acf17920
raniaw <raniaw@web.de>
2019-07-14 13:17:26 +0200

    new file

--- lesson69/js/script.js
+++ lesson69/js/script.js
@@ -1,7 +1,14 @@
+//
 var carousel_tiere = document.querySelector('.carousel-tiere');
+// Anzahl der Bildern -> 15
 var cellCount = 15;
 var selectedIndex = 0;
-
+/**
+ * Function für die Rotation des Tier-Karussels
+ * und für eine Berechnung  den Winkel
+ * fängt mit dem Winkel = 0° 
+ * dann erhöht sich bzw. verringert sich der Winkel auf 24° bzw. -24°
+ */
 function rotateCarousel() {
     var angle = selectedIndex / cellCount * (-360);
     carousel_tiere.style.transform = 'translateZ(-1411px) rotateY(' + angle + 'deg)';
